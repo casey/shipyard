3071ece87a9631531f9dc36ec8cdd1f921df1b48
Casey Rodarmor <casey@rodarmor.com>
2014-09-24 12:56:54 +0900

    use flotilla index handler

--- main.go
+++ main.go
@@ -3,8 +3,19 @@ package app
 import "net/http"
 import . "flotilla"
 
+var html = `<tt>
+  <a href="https://github.com/casey/shipyard">shipyard</a> for the
+  <a href="https://github.com/casey/flotilla">flotilla</a>
+</tt>`
+
 func init() {
-  Handle("/").Default(func(r *http.Request) {
-    Body(http.StatusOK, "Hi!\n", "text/plain; charset=utf-8")
-  })
+  Handle("/").Index(index).Default(handler)
+}
+
+func index(r *http.Request) {
+  Body(http.StatusOK, html, "text/html; charset=utf-8")
+}
+
+func handler(r *http.Request) {
+  Body(http.StatusOK, "Hi!\n", "text/plain; charset=utf-8")
 }
