09f19f268923429aa6f4f8b159ca387495f6ead7
Casey Rodarmor <casey@rodarmor.com>
2014-10-09 05:54:43 +0900

    drawing something

--- src/main.js
+++ src/main.js
@@ -2,14 +2,42 @@
 
 angular.module('app')
 
-.controller("main", function ($scope, $element) {
-  $element.find('noscript').remove();
-  // create and add canvas element
+.controller("main", function ($scope, $element, CT) {
+  $scope.images = [];
 
-  // get all drag and dropped images
-  // reduce them to a preview size
-  // draw them into the canvas
-  // download button
+  var ct = CT($element);
+
+  ct.fillStyle("#ffffaa").fillRect(0, 0, 500, 300);
+
+  $element.bind('dragover', function (e) {
+    e.preventDefault();
+  });
+
+  $element.bind('dragenter', function (e) {
+    e.preventDefault();
+  });
+
+  $element.bind('drop', function (e) {
+    e.preventDefault();
+    var files = e.dataTransfer.files;
+    for (var i = 0; i < files.length; i++) {
+      var img = document.createElement("img");
+      img.src = URL.createObjectURL(files[i]);
+      img.onload = function () {
+        $scope.images.push(this);
+        $scope.$digest();
+      }
+    }
+  });
+
+  $scope.$watchCollection('images', function (images) {
+    for (var i = 0; i < images.length; i++) {
+      var image = images[i];
+      console.log(image);
+      ct.drawImage(image, 0, 0);
+    }
+  }); 
+
 })
 
 ;
---
+++ src/util.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('app')
+
+.constant('CT', CT)
+
+;
