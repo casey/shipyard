1ee17bf2b607f43666b7e84a7321c866acf9907b
Casey Rodarmor <casey@rodarmor.com>
2014-10-10 19:17:23 +0900

    add angular to lib/ and add some sample code to src

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 'use strict';
 
-console.log('hi!');
+angular.module('app', []).controller('main', function ($scope, $interval) {
+  var excitement_level = 0;
+
+  $interval(function () {
+    excitement_level++;
+    $scope.excitement = Array(excitement_level).join("!");
+  }, 100);
+})
